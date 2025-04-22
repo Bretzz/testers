76943bacda5eab4c9d4e3f0ee8d9505b14509cb0
Tommaso Piana Agostinetti <topiana-@c2r4p8.42firenze.it>
2025-04-22 19:36:15 +0200

    more tests added

--- push_test.sh
+++ push_test.sh
@@ -2,6 +2,30 @@
 
 max=0;
 
+# 5 TEST
+for i in $(seq 1 200)
+do
+	ARG=$(shuf -i 0-2147483640 -n 5 | tr '\n' ' ')
+	moves=$(./push_swap $ARG | wc -l)
+	if [ "$moves" -ge "$max" ]; then
+        max=$moves
+    fi
+done
+echo "MAX[5  ]=$max";
+
+# 10 TEST
+for i in $(seq 1 200)
+do
+	ARG=$(shuf -i 0-2147483640 -n 10 | tr '\n' ' ')
+	moves=$(./push_swap $ARG | wc -l)
+	if [ "$moves" -ge "$max" ]; then
+        max=$moves
+    fi
+done
+echo "MAX[10 ]=$max";
+
+max=0;
+
 # 100 TEST
 for i in $(seq 1 100)
 do
@@ -15,7 +39,7 @@ echo "MAX[100]=$max";
 
 max=0;
 
-# 100 TEST
+# 500 TEST
 for i in $(seq 1 25)
 do
 	ARG=$(shuf -i 0-2147483640 -n 500 | tr '\n' ' ')
